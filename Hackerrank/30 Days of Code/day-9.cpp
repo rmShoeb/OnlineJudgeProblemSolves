32e96363bc9fdaaf5ae3c1a7a1592ea965537ddb
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-06-04 19:27:06 +0600

    Recursion-3

---
+++ Hackerrank/30 Days of Code/day-9.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+// start of my code
+int factorial(int n) {
+    static int facts[] = {1,1,2,0,0,0,0,0,0,0,0,0,0};
+
+    if(facts[n]==0){
+        facts[n] = n*factorial(n-1);
+    }
+    return facts[n];
+}
+// end of my code
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
