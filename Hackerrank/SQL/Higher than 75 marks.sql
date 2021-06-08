2434c0f024ad90ab84b9caf363bef7af832a323d
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-06-08 12:07:00 +0600

    Create Higher than 75 marks.sql

---
+++ Hackerrank/SQL/Higher than 75 marks.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT Name
+FROM Students
+Where Marks>75
+ORDER BY SUBSTR(Name, -3);
